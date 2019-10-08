76788f5eaf22ee91c5f98d791f069e3bd2c31783
Nick Riabovol <nick.riabovol@gmail.com>
2019-10-08 11:05:48 +0300

    test portlet

---
+++ src/main/java/com/exo/portlet/style/HelloPortlet.java
@@ -0,0 +1,12 @@
+package com.exo.portlet.style;
+
+import javax.portlet.*;
+import java.io.IOException;
+
+public class HelloPortlet extends GenericPortlet {
+    @RenderMode(name = "view")
+    public void Hello(RenderRequest request, RenderResponse response) throws IOException, PortletException {
+        PortletRequestDispatcher prDispatcher = getPortletContext().getRequestDispatcher("/jsp/hello.jsp");
+        prDispatcher.include(request, response);
+    }
+}
